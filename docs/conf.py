5c4c50105020cf11cb9b093cceac911fc000587e
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2026-06-25 15:24:47 -0300

    Preparing release v0.2

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,24 @@
-# Configuration file for the Sphinx documentation builder.
 #
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# conf.py
+#
+# Copyright The PyModulation Contributors.
+#
+# This file is part of PyModulation library.
+#
+# PyModulation library is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyModulation library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with PyModulation library. If not, see <http://www.gnu.org/licenses/>.
+#
+#
 
 import sphinx_rtd_theme
 
@@ -10,8 +27,8 @@ import sphinx_rtd_theme
 
 project = 'PyModulation'
 copyright = 'Copyright The PyModulation Contributors'
-author = 'Gabriel Mariano Marcelino'
-release = 'v0.1'
+author = 'PyModulation Developers'
+release = 'v0.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
